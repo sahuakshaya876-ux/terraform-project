be81fdd98c25d5deb47ee2e3df6a7468d91df6c4
Akshaya sahu <sahuakshaya876@gmail.com>
2026-08-19 15:34:41 +0530

    Rename ELB resource from 'raham' to 'akshaya'

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+  name               = "akshaya-terraform-elb"
   availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
